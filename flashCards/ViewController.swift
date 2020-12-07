c64ed4c8659b1bf118f6294151168367aa705aa4
SHY <ac60822@gmail.com>
2020-12-06 19:27:30 -0800

    Initial Commit

---
+++ flashCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashCards
+//
+//  Created by 游舒含 on 12/6/20.
+//  Copyright © 2020 ShuHan Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
